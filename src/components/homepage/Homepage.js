d7fefdc2cc477f313018fe293f3047e7e34470ec
Alora Riley <alora.riley@gmail.com>
2025-02-02 23:11:44 -0600

    feat: Attempt two at homepage styling.

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -17,27 +17,25 @@ const Homepage = () => {
   }, [userId, navigate])
 
   return (
-  <main className="homepage-main">
-    <header className="homepage-header">  
-      <h1 className="homepage-h1"> Journey Like a Genius </h1>
-        <button className="login-button" onClick={login} >Login!</button>
-    </header>
-    <section className="homepage-section">
-        <h2>
-          We're excited to get started on your personalized travel itinerary.
-        </h2>
-        <p>
-          We'll ask a series of questions to better understand you lifestyle,
-          preferences, and unique needs so we can generate a perfect travel day.
-        </p>
-        <Link to={`/preferences/${userId}`}>
-        <button className="preferences">Let's get started </button>
-        </Link>
-        <Link to={`/saved-itineraries/`}>
-        <button className="saved-itineraries">View Saved Itineraries</button>
-        </Link>
-      </section>
-    </main>
+    <div className="homepage-container">
+      <header className="homepage-header">  
+        <h1 className="homepage-h1">Journey Like a Genius</h1>
+      </header>
+      <main className="homepage-main">
+        <div className="background-image"></div>
+        <button className="login-button" onClick={login}>Login!</button>
+        <section className="homepage-section">
+          <h2>We're excited to get started on your personalized travel itinerary.</h2>
+          <p>We'll ask a series of questions to better understand you lifestyle, preferences, and unique needs so we can generate a perfect travel day.</p>
+          <Link to={`/preferences/${userId}`}>
+            <button className="preferences">Let's get started</button>
+          </Link>
+          <Link to={`/saved-itineraries/`}>
+            <button className="saved-itineraries">View Saved Itineraries</button>
+          </Link>
+        </section>
+      </main>
+    </div>
   );
 };
 
